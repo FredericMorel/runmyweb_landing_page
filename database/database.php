a7ec89090d4b49a8293b2a0297f1713e106c8256
RunMyWeb <runmyweb974@gmail.com>
2023-07-10 22:27:09 +0400

    first commit

---
+++ database/database.php
@@ -0,0 +1,11 @@
+<?php
+try {
+    $dbh = new PDO('mysql:host=localhost:3306;dbname=test', $user, $pass);
+    foreach($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
